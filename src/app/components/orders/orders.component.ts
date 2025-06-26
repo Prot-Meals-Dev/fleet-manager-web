079d96e7eb9289bedbeedb99a39cc1cb4751e311
cuddleminddev <cuddleminddev@gmail.com>
2025-06-26 11:57:58 +0530

    order list

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,11 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-orders',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css'
 })
 export class OrdersComponent {
-
+  orders = [
+    {
+      id: 201,
+      name: 'John Doe',
+      address: '123 Main Street, Mumbai',
+      startDate: '2025-06-20',
+      deliveryPartner: 'Ravi Kumar',
+      paymentStatus: 'Paid',
+      completed: true
+    },
+    {
+      id: 202,
+      name: 'Jane Smith',
+      address: '456 Lake View, Delhi',
+      startDate: '2025-06-21',
+      deliveryPartner: 'Anjali Singh',
+      paymentStatus: 'Unpaid',
+      completed: false
+    },
+    {
+      id: 203,
+      name: 'David Johnson',
+      address: '789 Hilltop Rd, Bangalore',
+      startDate: '2025-06-22',
+      deliveryPartner: 'Amit Sharma',
+      paymentStatus: 'Paid',
+      completed: false
+    }
+  ];
 }
