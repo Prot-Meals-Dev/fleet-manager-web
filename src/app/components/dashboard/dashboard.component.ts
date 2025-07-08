3d620b3869546c2fc8b52059ea1b04434fad5629
cuddleminddev <cuddleminddev@gmail.com>
2025-07-08 10:45:01 +0530

    dashboard recent activities

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,30 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from './service/dashboard.service';
+import { CommonModule } from '@angular/common';
+import { AlertService } from '../../shared/components/alert/service/alert.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
   allData!: any;
+  allDeliveries!: any;
 
   constructor(
-    private service: DashboardService
+    private service: DashboardService,
+    private alertService: AlertService
   ) { }
 
   ngOnInit(): void {
     this.loadData()
+    this.loadDeliveries()
   }
 
   loadData() {
     this.service.getAnalytics().subscribe({
-      next: (res:any) => {
+      next: (res: any) => {
         this.allData = res.data || {}
       },
       error: (err) => {
         console.error(err);
+        this.alertService.showAlert({
+          message: err.error.message || 'Update failed',
+          type: 'error',
+          autoDismiss: true,
+          duration: 4000
+        });
+      }
+    })
+  }
+
+  loadDeliveries() {
+    this.service.getDeliveries().subscribe({
+      next: (res: any) => {
+        this.allDeliveries = res.data || []
+      },
+      error: (err) => {
+        console.error(err);
+        this.alertService.showAlert({
+          message: err.error.message,
+          type: 'error',
+          autoDismiss: true,
+          duration: 4000
+        });
       }
     })
   }
--- src/app/components/dashboard/service/dashboard.service.ts
+++ src/app/components/dashboard/service/dashboard.service.ts
@@ -7,12 +7,19 @@ import { HttpClient } from '@angular/common/http';
 })
 export class DashboardService {
   private BaseUrl = `${environment.apiUrl}/fleet-manager/analytics`
+  private DeliveriesUrl = `${environment.apiUrl}/delivery-assignments`
 
   constructor(
     private http: HttpClient
   ) { }
 
-  getAnalytics(){
+  getAnalytics() {
     return this.http.get(`${this.BaseUrl}`)
+  }
+
+  getDeliveries() {
+    const today = new Date();
+    const formattedDate = today.toISOString().split('T')[0]; // YYYY-MM-DD
+    return this.http.get(`${this.DeliveriesUrl}/my-deliveries?date=${formattedDate}`);
   }
 }
